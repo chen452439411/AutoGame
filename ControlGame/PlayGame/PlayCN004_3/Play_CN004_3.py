203aa008df184e8e0672612531ed7a2f3fe5a023
chen <452439411@qq.com>
2022-04-02 16:52:15 +0800

    更新cn004脚本

---
+++ ControlGame/PlayGame/PlayCN004_3/Play_CN004_3.py
@@ -0,0 +1,40 @@
+# -*- encoding=utf8 -*-
+__author__ = "chenjiarui"
+
+from ControlGame.PlayGame.Common import *
+from ControlGame.common import *
+
+
+def play1():
+    click_attack2()
+    click_attack_centre()
+
+
+def play2():
+    click_attack3()
+    click_attack_centre()
+
+
+def play3():
+    click_attack4()
+    click_attack_centre()
+
+
+def play4():
+    click_attack5()
+    click_attack_centre()
+
+
+def play5():
+    click_attack6()
+    click_attack_centre()
+
+
+def main():
+    close_view()
+    qianjin()
+    play1()
+    play2()
+    play3()
+    play4()
+    play5()
